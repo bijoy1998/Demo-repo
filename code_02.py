69fa7f72c983cbbe0adfeea0f3d298b576a41145
bijoy1998 <bijoysaha588@gmail.com>
2025-04-18 20:48:21 +0600

    Update code_02

--- code_02.py
+++ code_02.py
@@ -1,6 +1,8 @@
-a = 4
-b = 5
+# Addition of two real numbers
+
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the second number: "))
 
 c = a+b
 
-print(c)
+print("Sum of a and b: ",c)
